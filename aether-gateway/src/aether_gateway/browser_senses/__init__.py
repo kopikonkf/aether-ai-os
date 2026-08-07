d249659a3abba8c8dfc517611ef21e0a34227a7d
kopikonkf <kopiko.nkf@gmail.com>
2026-08-07 09:04:28 +0000

    feat(senses): export bootstrap service

--- aether-gateway/src/aether_gateway/browser_senses/__init__.py
+++ aether-gateway/src/aether_gateway/browser_senses/__init__.py
@@ -1,4 +1,13 @@
 """Browser media transport, LiveKit token issuance, and Aether sense bridge."""
+
+from .bootstrap import (
+    BootstrapError,
+    BootstrapRateLimitError,
+    BootstrapStateError,
+    BrowserSenseBootstrapService,
+    DeviceCredentialError,
+    SessionCredentialError,
+)
 from .service import (
     BrowserSenseAuthError,
     BrowserSenseService,
@@ -7,8 +16,14 @@ from .service import (
 )
 
 __all__ = [
+    "BootstrapError",
+    "BootstrapRateLimitError",
+    "BootstrapStateError",
     "BrowserSenseAuthError",
+    "BrowserSenseBootstrapService",
     "BrowserSenseService",
     "BrowserSessionTokenCodec",
+    "DeviceCredentialError",
     "LiveKitTokenIssuer",
+    "SessionCredentialError",
 ]
